04cbda0e66afca737cd8a1c0986c5978fc1fd856
BluesFan44 <bluesfan44@gmail.com>
2023-05-14 18:00:47 -0500

    Add Item Count to Cart

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
-import { Fragment } from "react";
+import { useState } from "react";
 import Header from "./Components/Layout/header";
 import Meals from "./Components/Meals/Meals";
+import Cart from "./Components/Cart/cart";
+import CartProvider from "./store/cart-provider";
 
 function App() {
+  const [cartIsShown, setCartIsShown] = useState();
+
+  const showCartHandller = () => {
+    setCartIsShown(true);
+  };
+
+  const hideCartHandller = () => {
+    setCartIsShown(false);
+  };
+
   return (
-    <Fragment>
-      <Header />
+    <CartProvider>
+      {cartIsShown && <Cart onClose={hideCartHandller} />}
+      <Header onShowCart={showCartHandller} />
       <main>
         <Meals />
       </main>
-    </Fragment>
+    </CartProvider>
   );
 }
 
